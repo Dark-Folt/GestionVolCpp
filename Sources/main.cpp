2c2d81b89dad8654386b2c57bf7a4a58581c0240
Dark-Folt <marigo9794@gmail.com>
2020-11-10 16:48:05 +0100

    creation du fichier main.cpp

---
+++ Sources/main.cpp
@@ -0,0 +1,16 @@
+#include "Reservation.cpp"
+#include "Vol.cpp"
+#include "DateT.cpp"
+#include "Destination.cpp"
+
+using namespace std;
+int main()
+{
+    // Reservation r1("125FCQ","675FGDF","FYEGFZE",true);
+    Reservation r;
+    Destination d1;
+    DateT da;
+    // Vol v1("65DZAF",6467,4.5,&d1,&da);
+    Vol v1;
+    return 0;
+}
