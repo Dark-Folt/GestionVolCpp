d0bf0eea582ae40634cd4b89783c9f6efcbe79d4
Dark-Folt <marigo9794@gmail.com>
2020-11-10 16:45:29 +0100

    ajout de la classe DateT

--- Headers/DateT.hpp
+++ Headers/DateT.hpp
@@ -4,6 +4,31 @@
 class DateT
 {
     private:
+        unsigned int jour;
+        unsigned int moi;
+        unsigned int annee;
+        long heures;
+        long minutes;
+
     public:
+        //Constructeurs
+        DateT();
+        DateT(unsigned int, unsigned int, unsigned int, unsigned long, unsigned  long);
+        ~DateT();
+
+        //getters
+        unsigned int getJour();
+        unsigned int getMoi();
+        unsigned int getAnnee();
+        long getHeures();
+        long getMinutes();
+
+        //setters
+        void setJour(unsigned int);
+        void setMoi(unsigned int);
+        void setAnnee(unsigned int);
+        void setHeures(unsigned long);
+        void setMinutes(unsigned long);
+
 };
 #endif
